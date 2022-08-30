699667c02170c945cad5aabab35661fc3cb34514
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-08-30 23:30:22 +0530

    create a table emp payroll service in database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,6 @@
 ---UC1 create Database payroll Service
 create database Payroll_service;
+--UC2
+use Payroll_service;
+create table payroll_service(empid int identity(1,1)primary key,empname varchar(200),salary bigint,start_date date);
+select*from Payroll_service;
