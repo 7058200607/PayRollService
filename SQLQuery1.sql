5b15857de2151287305bb839c5b8a7fcf41f94e2
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-09-01 18:24:58 +0530

    Insert a new employee data in table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,3 +4,9 @@ create database Payroll_service;
 use Payroll_service;
 create table payroll_service(empid int identity(1,1)primary key,empname varchar(200),salary bigint,start_date date);
 select*from Payroll_service;
+--UC3
+insert into payroll_service values('Bill',1000000,'2018-01-03');
+insert into payroll_service values('Terisa',3000000,'2019-11-13');
+insert into payroll_service values('charlie',3000000,'2020-05-21');
+--UC4 Retrieve all employee Data
+select*from payroll_service;
