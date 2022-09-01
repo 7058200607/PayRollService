8ab8086d4a9390118ad5a4eea64e9bd62e363529
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-09-01 18:38:10 +0530

    salary data of perticular person and perticular person

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,3 +10,6 @@ insert into payroll_service values('Terisa',3000000,'2019-11-13');
 insert into payroll_service values('charlie',3000000,'2020-05-21');
 --UC4 Retrieve all employee Data
 select*from payroll_service;
+--UC5 salary data of perticular person and perticular person
+select salary from payroll_service where empname='bill';
+select empname from payroll_service where start_date between '2019-01-01' and GETDate();
