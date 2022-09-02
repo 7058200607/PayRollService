311910d569110012e567cef057149139f2b61eb4
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-09-02 18:52:33 +0530

    Add gender coloumn in payroll service

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,6 +10,10 @@ insert into payroll_service values('Terisa',3000000,'2019-11-13');
 insert into payroll_service values('charlie',3000000,'2020-05-21');
 --UC4 Retrieve all employee Data
 select*from payroll_service;
---UC5 salary data of perticular person and perticular person
+--UC5 salary data of perticular person and date in perticular range of person
 select salary from payroll_service where empname='bill';
 select empname from payroll_service where start_date between '2019-01-01' and GETDate();
+--UC6 add gender coloumn
+alter table payroll_service add gender char;
+update payroll_service set gender='m' where empname='Bill' or empname='charlie';
+update payroll_service set gender ='f' where empname='Terisa'
